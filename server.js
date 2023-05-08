e4cf94e4d4c4fca549be2f01572404c77490768b
Grayson St. Aubin <gsa@unc.edu>
2023-05-08 12:11:58 -0400

    Added a04 code to server.js

--- server.js
+++ server.js
@@ -71,6 +71,52 @@ app.use(morgan(':remote-addr - :remote-user [:date[iso]] ":method :url HTTP/:htt
 // Serve static files
 const staticpath = args.stat || args.s || process.env.STATICPATH || path.join(__dirname, 'public')
 app.use('/', express.static(staticpath))
+
+import { rps, rpsls } from './lib/rpsls.js';
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/app/', function(req, res) {
+    res.status(200).send('200 OK');
+});
+
+app.get('/app/rps/', function(req, res) {
+    res.status(200).send(JSON.stringify(rps()));
+});
+
+app.get('/app/rpsls/', function(req, res) {
+    res.status(200).send(JSON.stringify(rpsls()));
+});
+
+app.get('/app/rps/play/', function(req, res) {
+    res.status(200).send(JSON.stringify(rps(req.query.shot)));
+});
+
+app.post('/app/rps/play/', function(req, res) {
+    res.status(200).send(JSON.stringify(rps(req.body.shot)));
+});
+
+app.get('/app/rps/play/:shot/', function(req, res) {
+    res.status(200).send(JSON.stringify(rps(req.params.shot)));
+});
+
+app.get('/app/rpsls/play/', function(req, res) {
+    res.status(200).send(JSON.stringify(rpsls(req.query.shot)));
+});
+
+app.post('/app/rpsls/play/', function(req, res) {
+    res.status(200).send(JSON.stringify(rpsls(req.body.shot)));
+});
+
+app.get('/app/rpsls/play/:shot/', function(req, res) {
+    res.status(200).send(JSON.stringify(rpsls(req.params.shot)));
+});
+
+app.get('/*', function(req, res) {
+    res.status(404).send('404 NOT FOUND');
+});
+
 // Create app listener
 const server = app.listen(port)
 // Create a log entry on start
