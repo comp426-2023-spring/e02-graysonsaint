26d46276f13284b675529de3c3934283ce6b16a5
Grayson St. Aubin <gsa@unc.edu>
2023-05-08 18:42:34 -0400

    Fixed rules.html, scripts.js, and styles.css

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,84 @@
 // If you would like to see some examples of similar code to make an interface interact with an API, 
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
+
+function play() {
+    var rps = document.getElementById('rps').checked;
+    var rpsls = document.getElementById('rpsls').checked;
+    var opp = document.getElementById('opp').checked;
+    var shot;
+    var shots = document.getElementsByName('shot-choice');
+    for (var i = 0; i < shots.length; i++) {
+        if (shots[i].checked) {
+            shot = shots[i].value;
+            break;
+        }
+    }
+
+    if (!rps && !rpsls) {
+        document.getElementById('result').innerText = 'Choose a game mode.';
+    } else if (rps && !opp) {
+        //document.getElementById('result').innerText = 'RPS';
+        fetch(`/app/rps/play`)
+            .then((res) => {
+                return res.json();
+            })
+            .then((json) => {
+                document.getElementById('result').innerText = `Shot: ${json.player}`;
+            })
+    } else if (rpsls && !opp) {
+        //document.getElementById('result').innerText = 'RPSLS';
+        
+        fetch(`/app/rpsls/play`)
+            .then((res) => {
+                return res.json();
+            })
+            .then((json) => {
+                document.getElementById('result').innerText = `Shot: ${json.player}`;
+            })
+    } else if (rps && opp) {
+        fetch(`/app/rps/play/${shot}`)
+            .then((res) => {
+                return res.json();
+            })
+            .then((json) => {
+                document.getElementById('result').innerText = `Player: ${json.player}\nOpponent: ${json.opponent}\nResult: ${json.result}`;
+            })
+    } else {
+        fetch(`/app/rpsls/play/${shot}`)
+            .then((res) => {
+                return res.json();
+            })
+            .then((json) => {
+                document.getElementById('result').innerText = `Player: ${json.player}\nOpponent: ${json.opponent}\nResult: ${json.result}`;
+            })
+    }
+    document.getElementById('results').className = 'show';
+    
+}
+
+function showShots() {
+    var rps = document.getElementById('rps').checked;
+    var rpsls = document.getElementById('rpsls').checked;
+    var opp = document.getElementById('opp').checked;
+    if(opp) {
+        if (rps) {
+            document.getElementById('rpsshots').className = 'show';
+            document.getElementById('rpslsshots').className = 'hide';
+        } else if (rpsls) {
+            document.getElementById('rpsshots').className = 'show';
+            document.getElementById('rpslsshots').className = 'show';
+        }
+    } else {
+        document.getElementById('rpsshots').className = 'hide';
+        document.getElementById('rpslsshots').className = 'hide';
+    }
+}
+
+function restart() {
+    document.getElementById('results').className = 'hide';
+    document.getElementById('rpsshots').className = 'hide';
+    document.getElementById('rpslsshots').className = 'hide';
+    document.getElementById('rps').checked = true;
+    document.getElementById('opp').checked = false;
+}
--- server.js
+++ server.js
@@ -72,6 +72,38 @@ app.use(morgan(':remote-addr - :remote-user [:date[iso]] ":method :url HTTP/:htt
 const staticpath = args.stat || args.s || process.env.STATICPATH || path.join(__dirname, 'public')
 app.use('/', express.static(staticpath))
 
+// Create app listener
+const server = app.listen(port)
+// Create a log entry on start
+let startlog = new Date().toISOString() + ' HTTP server started on port ' + port + '\n'
+// Debug echo start log entry to STDOUT
+if (args.debug) {
+    console.info(startlog)
+} 
+// Log server start to file
+fs.appendFileSync(path.join(logpath, 'server.log'), startlog)
+// Exit gracefully and log
+process.on('SIGINT', () => {
+// Create a log entry on SIGINT
+    let stoppinglog =  new Date().toISOString() + ' SIGINT signal received: stopping HTTP server\n'
+//  Log SIGINT to file
+    fs.appendFileSync(path.join(logpath, 'server.log'), stoppinglog)
+// Debug echo SIGINT log entry to STDOUT
+    if (args.debug) {
+        console.info('\n' + stoppinglog)
+    }
+// Create a log entry on stop
+    server.close(() => {
+        let stoppedlog = new Date().toISOString() + ' HTTP server stopped\n'
+// Log server stop to file
+        fs.appendFileSync(path.join(logpath, 'server.log'), stoppedlog)
+// Debug echo stop log entry to STDOUT
+        if (args.debug) {
+            console.info('\n' + stoppedlog)
+        }    
+    })
+})
+
 import { rps, rpsls } from './lib/rpsls.js';
 
 app.use(express.json());
@@ -116,35 +148,3 @@ app.get('/app/rpsls/play/:shot/', function(req, res) {
 app.get('/*', function(req, res) {
     res.status(404).send('404 NOT FOUND');
 });
-
-// Create app listener
-const server = app.listen(port)
-// Create a log entry on start
-let startlog = new Date().toISOString() + ' HTTP server started on port ' + port + '\n'
-// Debug echo start log entry to STDOUT
-if (args.debug) {
-    console.info(startlog)
-} 
-// Log server start to file
-fs.appendFileSync(path.join(logpath, 'server.log'), startlog)
-// Exit gracefully and log
-process.on('SIGINT', () => {
-// Create a log entry on SIGINT
-    let stoppinglog =  new Date().toISOString() + ' SIGINT signal received: stopping HTTP server\n'
-//  Log SIGINT to file
-    fs.appendFileSync(path.join(logpath, 'server.log'), stoppinglog)
-// Debug echo SIGINT log entry to STDOUT
-    if (args.debug) {
-        console.info('\n' + stoppinglog)
-    }
-// Create a log entry on stop
-    server.close(() => {
-        let stoppedlog = new Date().toISOString() + ' HTTP server stopped\n'
-// Log server stop to file
-        fs.appendFileSync(path.join(logpath, 'server.log'), stoppedlog)
-// Debug echo stop log entry to STDOUT
-        if (args.debug) {
-            console.info('\n' + stoppedlog)
-        }    
-    })
-})
